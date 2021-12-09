6fbe7509ce393cbddb466856051f3e47e3e5bcd8
David Keller <kellerdb@gmail.com>
2021-12-09 08:34:24 -0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# chatbot-python
+
+Chatbot app written in Python.
+
+Use this app at [trinket.io](https://trinket.io/embed/python3/5aace03e8c?outputOnly=true&start=result).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ main.py
@@ -0,0 +1,12 @@
+print()
+
+name = input("What is your name? ")
+print(f"Hello {name}, my name is Chatbot 3000.\n")
+
+food = input("What is your favorite food? ")
+print(f"I think {food} sounds tasty, but I prefer electrons.\n")
+
+hobby = input("What is your favorite hobby? ")
+print(f"{hobby} is fun! My favorite hobby is chatting.\n")
+
+print("Thanks for talking, goodbye!")
